7054f3841dedfde51907c7030001f51a30f279fc
Turpin Yannick <TurpinYa@gmail.com>
2022-05-09 16:53:24 +0200

    store

---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import {configureStore} from '@reduxjs/toolkit';
+import UpdateReducer from './reducer/UpdateReducer';
+
+
+
+
+const store = configureStore({reducer: UpdateReducer});
+
+export default store;
