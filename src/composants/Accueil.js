08a1fb6549593ebbde6a9aab140a21f28420c286
gaetan <gs99@hotmail.fr>
2022-05-09 16:53:52 +0200

    axios get

--- src/composants/Accueil.js
+++ src/composants/Accueil.js
@@ -1,16 +1,43 @@
 import { Component } from "react"
+import axios from 'axios'
 
 class Accueil extends Component {
     constructor(props) {
         super(props);
+        this.state = { 
+            listeArticles: [],
+            nosArticles: []
+        }
     }
-    state = {  }
     render() { 
         return ( 
         <div>
-
+            {this.state.nosArticles.map(e=>{
+                return(
+                    <>
+                        <p>{e.title}</p>
+                        <button>Lien</button>
+                    </>
+                )
+            })}
+            {this.state.listeArticles.map(e=>{
+                return(
+                    <>
+                        <p>{e.title}</p>
+                        <button>Lien</button>
+                    </>
+                )
+            })}
         </div> 
         );
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+          const articles = res.data;
+          console.log(articles)
+          this.setState({ nosArticles: articles });
+        })
     }
 }
  
